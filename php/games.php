8fc483ccc506722b56b52c6124980360926fd7fc
TommasoIntini <47627884+TommasoIntini@users.noreply.github.com>
2020-01-17 17:43:57 +0100

    Games pushed into a json array

--- php/games.php
+++ php/games.php
@@ -1,13 +1,19 @@
 <?php
 	$files = scandir("./games");
 	
+	$arr = array();
+	
 	foreach($files as $file){
 		if(!is_dir($file)){
 			$info = pathinfo($file);
 			if($info['extension'] == "php"){
-				echo $info['filename']."<br>";
+				array_push($arr,$info['filename']);
 			}
 		}
 	}
+	
+	$jsonData = json_encode($arr);
+
+	echo $jsonData;
 	
 ?>
