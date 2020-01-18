f195988a340519c761d89b334d0a24d9a026d823
Tommaso <tommasointini01@gmail.com>
2020-01-18 18:57:02 +0100

    Add Http ScoreService and JsonCardsSerializer

--- android/app/src/main/java/gov/ismonnet/cardhelp/http/HttpScoreService.java
+++ android/app/src/main/java/gov/ismonnet/cardhelp/http/HttpScoreService.java
@@ -1,7 +1,12 @@
 package gov.ismonnet.cardhelp.http;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.UncheckedIOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Collection;
 
 import javax.inject.Inject;
@@ -20,14 +25,39 @@ class HttpScoreService implements ScoreService {
 
     @Override
     public int calculatePoints(String game, Collection<Card> cards) {
+
+        String serialized = cardsSerializer.serialize(game, cards);
+
         try {
             // HttpURLConnection post
-            if(false)
-                throw new IOException("temp");
+            URL url = new URL("http://localhost/Tommaso/php/score.php");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-            return -1;
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+
+            OutputStream out = connection.getOutputStream();
+            out.write(serialized.getBytes("UTF-8"));
+            out.close();
+
+            StringBuilder response = new StringBuilder();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+
+            while ((line = rd.readLine()) != null){
+                response.append(line);
+            }
+            rd.close();
+
+            try{
+                return Integer.parseInt(response.toString());
+            }catch(NumberFormatException e){
+                return -1;
+            }
         } catch (IOException ex) {
             throw new UncheckedIOException(ex);
         }
+
     }
 }
--- android/app/src/main/java/gov/ismonnet/cardhelp/serializer/JsonCardsSerializer.java
+++ android/app/src/main/java/gov/ismonnet/cardhelp/serializer/JsonCardsSerializer.java
@@ -5,6 +5,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Collection;
+import java.util.Vector;
 
 import javax.inject.Inject;
 
@@ -17,8 +18,51 @@ class JsonCardsSerializer implements CardsSerializer {
 
     @Override
     public String serialize(String game, Collection<Card> cards) {
+        //TODO: When the application can detect cards, remove initialization
+        cards = new Vector<>();
 
+        cards.add(new Card(Card.Suit.CLUB,4));
+        cards.add(new Card(Card.Suit.HEART,3));
+        cards.add(new Card(Card.Suit.DIAMOND,12));
+        cards.add(new Card(Card.Suit.SPADE,5));
 
+        JSONObject jRes = new JSONObject();
+        JSONObject jTemp = new JSONObject();
+
+        JSONArray jCards = new JSONArray();
+
+        try {
+            //put game in JSON result
+            jRes.put("game", game);
+
+            cards.forEach(card -> {
+                try {
+                    //create new card object
+                    jTemp.put("number",card.getNumber());
+                    jTemp.put("suit",card.getSuit().toString());
+
+                    //put card to array
+                    jCards.put(new JSONObject(jTemp.toString()));
+                    //remove existing card object
+
+                    jTemp.remove("suit");
+                    jTemp.remove("number");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } );
+
+            //put vector into result
+            jRes.put("cards",jCards);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return jRes.toString();
+    }
+}
 
         /*
         org.json.JSONObject
@@ -70,6 +114,4 @@ class JsonCardsSerializer implements CardsSerializer {
         }'
         */
 
-        return null;
-    }
-}
+
